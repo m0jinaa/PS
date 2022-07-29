0e674129f9229389a0fffb52fe18f3a667f74822
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2022-07-30 00:19:43 +0900

    [Bronze IV] Title: Tri-du, Time: 212 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/13597. Tri－du/Tri－du.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if(a==b) {
+			System.out.println(a);
+		}
+		else {
+			System.out.println(Math.max(a, b));
+		}
+	}
+
+}
