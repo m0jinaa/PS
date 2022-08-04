66c7f2e38da03591949240caf79950da6d80a148
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2022-08-04 13:02:12 +0900

    [Silver III] Title: N과 M (2), Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,22 @@
+def chose(g,n,m):
+    if len(g) == m:
+        for t in g:
+            print(t,end=" ")
+        print()
+    else:
+        if len(g)>=1:
+            for i in range(max(g),n+1):
+                if i not in g:
+                    g.append(i)
+                    chose(g,n,m)
+                    g.pop()
+        else:
+            for i in range(1,n+1):
+                if i not in g:
+                    g.append(i)
+                    chose(g,n,m)
+                    g.pop()
+
+n,m = map(int,input().split())
+graph = []
+chose(graph,n,m)
