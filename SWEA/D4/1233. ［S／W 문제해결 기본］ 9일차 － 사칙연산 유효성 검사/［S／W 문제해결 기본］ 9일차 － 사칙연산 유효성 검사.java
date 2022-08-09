a6f7e9c14e33804c466ff9f4cb31ca9119fdf078
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2022-08-09 10:18:35 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 115 ms, Memory: 21,120 KB -BaekjoonHub

--- SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -1,46 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
-class Node{
-	Boolean isNumber;
-	int left = -1;
-	int right = -1;
-	
-	public Node(Boolean isNumber) {
-		this.isNumber = isNumber;
-	}
-
-	public Node(Boolean isNumber, int left) {
-		this.isNumber = isNumber;
-		this.left = left;
-	}
-
-	public Node(Boolean isNumber, int left, int right) {
-		this.isNumber = isNumber;
-		this.left = left;
-		this.right = right;
-	}
-	
-}
 public class Solution {
-	static Node[] tree;
-	static boolean[] result;
-	static int index;
-	static boolean answer;
-	static void inOrder(int start) {
-		if(start == -1||answer == false)
-			return;
-		inOrder(tree[start].left);
-		if(index == 0||result[index-1]!=tree[start].isNumber)
-			result[index++] = tree[start].isNumber;
-		else {
-			answer = false;
-			return;
-		}
-		inOrder(tree[start].right);
-	}
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -48,41 +12,53 @@ public class Solution {
 		for(int t = 1;t<=10;t++) {
 			int n = Integer.parseInt(br.readLine());
 			StringTokenizer st;
-			tree = new Node[n+1];
-			index = 0;
-			result = new boolean[n];
-			answer = true;
+			boolean answer = true;
+			
+			int[][] tree = new int[n+1][];
+			int left = -1;
+			int right = -1;
+			
 			for(int i = 0;i<n;i++) {
 				st = new StringTokenizer(br.readLine()," ");
 				int m = Integer.parseInt(st.nextToken());
-
-				String token = st.nextToken();
-				char c = token.charAt(0);
+				String node = st.nextToken();
+				int val = 1;
+				if(node.equals("*")||node.equals("/")||node.equals("-")||node.equals("+"))
+					val = 0;
+				left = -1;
+				right = -1;
 				if(st.hasMoreTokens()) {
-					int left = Integer.parseInt(st.nextToken());
+					left = Integer.parseInt(st.nextToken());
 					if(st.hasMoreTokens()) {
-						int right = Integer.parseInt(st.nextToken());
-						if(token.length()==1 && (c==42||c==43||c==45||c==47))//연산자일 경우
-							tree[m] = new Node(false,left,right);
-						else
-							tree[m] = new Node(true,left,right);
+						right = Integer.parseInt(st.nextToken());
 					}
+				}
+				tree[m] = new int[] {val,left,right};
+			}
+			
+			Stack<Integer> stack = new Stack<Integer>();
+			stack.push(1);
+			boolean[] visited = new boolean[n+1];
+			int isNumber = 0;
+			while(!stack.isEmpty()) {
+				int now = stack.pop();
+				if(visited[now]) {
+					if(tree[now][0]!=isNumber)
+						isNumber = 1-isNumber;
 					else {
-						if(token.length()==1 && (c==42||c==43||c==45||c==47))//연산자일 경우
-							tree[m] = new Node(false,left);
-						else
-							tree[m] = new Node(true,left);						
+						answer = false;
+						break;
 					}
 				}
 				else {
-					if(token.length()==1 && (c==42||c==43||c==45||c==47))//연산자일 경우
-						tree[m] = new Node(false);
-					else
-						tree[m] = new Node(true);				
-				}
+					visited[now] = true;
+					if(tree[now][2]!=-1)
+						stack.push(tree[now][2]);
+					stack.push(now);
+					if(tree[now][1]!=-1)
+						stack.push(tree[now][1]);
+				}					
 			}
-			inOrder(1);
-			
 			if(answer)
 				sb.append("#"+t+" 1\n");
 			else
