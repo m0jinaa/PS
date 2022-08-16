141526a603d1f9807d41d289d734ab2ffb2f9fc4
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2022-08-16 20:36:43 +0900

    [Gold II] Title: 트리의 순회, Time: 1492 ms, Memory: 42080 KB -BaekjoonHub

---
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] inOrder;
+	static int[] postOrder;
+	static StringBuilder sb = new StringBuilder();
+	static void preOrder(int i_start,int i_end,int p_start,int p_end) {
+		//System.out.println(i_start+ " "+i_end+ " "+p_start+ " "+p_end);
+		if(p_end<p_start)
+			return;
+		
+		int p = postOrder[p_end];
+		sb.append(p).append(" ");
+		//System.out.println(p);
+		int ind = 0;
+		for(int i = i_start;i<=i_end;i++) {
+			if(inOrder[i] == p) {
+				ind = i;
+				break;
+			}
+		}
+		preOrder(i_start,ind-1,p_start,p_start+(ind-1-i_start));
+		preOrder(ind+1,i_end,p_start+(ind-i_start),p_end-1);	
+	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+		inOrder = new int[n];
+		postOrder = new int[n];
+		st = new StringTokenizer(br.readLine()," ");
+		for(int i = 0;i<n;i++) {
+			inOrder[i] = Integer.parseInt(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine()," ");
+		for(int i = 0;i<n;i++) {
+			postOrder[i] = Integer.parseInt(st.nextToken());
+		}
+	
+		preOrder(0,n-1,0,n-1);
+		
+		System.out.println(sb);
+    }
+}
