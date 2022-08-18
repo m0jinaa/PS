0ad4d8c2e589d1d40f8c1b86c64845dae528e0b3
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2022-08-18 12:56:26 +0900

    [Gold II] Title: 빵집, Time: 408 ms, Memory: 34672 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -4,25 +4,22 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] v;
 	static char[][] map;
+	static int r,c;
 	public static boolean getPath(int a,int b) {
-		if(0>a ||a>=map.length||v[a][b] == 1||v[a][b] == -1)
-			return false;
-		else if(0<=a && a<map.length&&0<=b&&b<map[0].length&&map[a][b] == '.') {
-			if(b==map[0].length-1) {
-				v[a][b] = 1;
-				return true;
-			}
-			else{
-				if(getPath(a-1,b+1) ||getPath(a,b+1) ||getPath(a+1,b+1)) {
-					v[a][b] = 1;
-					return true;
-				}
-				else
-					v[a][b] = -1;
-			}
-		}
+		map[a][b] = '-';
+		
+		if(b==c-1)
+			return true;
+		
+		if(a>0 && map[a-1][b+1]=='.'&&getPath(a-1,b+1))
+			return true;
+		
+		if(map[a][b+1]=='.'&&getPath(a,b+1))
+			return true;
+		
+		if(a<r-1&&map[a+1][b+1]=='.'&&getPath(a+1,b+1))
+			return true;
 		
 		return false;
 		
@@ -32,14 +29,14 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int r = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
 		
 		map = new char[r][];
 		for(int i = 0;i<r;i++) {
 			map[i] = br.readLine().toCharArray();
 		}
-		v = new int[r][c];
+
 		int answer = 0;
 		for(int i = 0;i<r;i++) {
 			if(getPath(i,0))
