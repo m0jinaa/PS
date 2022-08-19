6542373d28210859e4883468761a2e7c661edca8
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2022-08-20 00:32:12 +0900

    [Silver II] Title: 과일 서리, Time: 160 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/17213. 과일 서리/과일 서리.java
+++ 백준/Silver/17213. 과일 서리/과일 서리.java
@@ -5,27 +5,27 @@ import java.io.InputStreamReader;
 public class Main {
 	static int n;
 	static int m;
+	static int target;
 	static int answer;
 	
 	public static void combi(int i,int cnt) {
-		if(cnt== m) {
-			if(i==n)
-				answer++;
+		if(cnt == target) {
+			answer++;
 			return;
 		}
 		else {
-			for(int j = i;j<=i+1 &&i+1<=n;j++) {
+			for(int j = i;j<n;j++) {
 				combi(j,cnt+1);
 			}
 		}
 	}
-	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		m = Integer.parseInt(br.readLine());
-		
+		target = m-n;
+
 		combi(0,0);
 		
 		System.out.println(answer);	
