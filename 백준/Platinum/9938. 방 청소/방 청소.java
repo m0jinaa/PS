44f5f18d2360462b2297b550217fb5f7df00d344
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2022-08-23 22:39:47 +0900

    [Platinum III] Title: 방 청소, Time: 1012 ms, Memory: 99636 KB -BaekjoonHub

---
+++ 백준/Platinum/9938. 방 청소/방 청소.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n,l;
+	static int[] root;
+	static boolean[] filled;
+	static int find(int x) {
+		if(root[x]!=x)
+			root[x] = find(root[x]);
+		return root[x];
+	}
+	static void union(int a, int b) {
+		root[find(a)] = find(b);
+	}
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		StringBuilder sb = new StringBuilder();
+		n = Integer.parseInt(st.nextToken());
+		l = Integer.parseInt(st.nextToken());
+		filled = new boolean[l+1];
+		root = new int[l+1];
+		for(int i = 0;i<=l;i++) {
+			root[i] = i;
+		}
+		int a,b;
+		boolean pos;
+		for(int i = 0;i<n;i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			pos = true;
+			if(!filled[a]) {//a에 넣을 수 있는지 확인
+				filled[a] = true;
+				union(a,b);
+			}
+			else if(!filled[b]) {
+				filled[b] = true;
+				union(b,a);
+			}
+			else if(!filled[find(a)]) {
+				filled[find(a)] = true;
+				union(a,b);
+			}
+			else if(!filled[find(b)]) {
+				filled[find(b)] = true;
+				union(b,a);
+			}
+			else {
+				pos = false;
+			}
+			if(pos) {
+				sb.append("LADICA").append("\n");
+			}
+			else {
+				sb.append("SMECE").append("\n");
+			}			
+		}
+		System.out.print(sb);
+	}
+}
