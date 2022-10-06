1628826600c3ed58f9b842514b7a22689ea8179b
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2022-10-06 16:10:21 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 84 ms, Memory: 11900 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -3,35 +3,31 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main{
-	static int n;
-	static int[] nums;
-	static int[] cnt;
+public class Main {
 	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		n = Integer.parseInt(br.readLine());
-		nums = new int[n];
-		
+		int n = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine()," ");
+		int[] arr = new int[n];
+		int[] C = new int[n];
+		
 		for(int i = 0;i<n;i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		cnt = new int[n];
-		Arrays.fill(cnt, 1);
-		cnt[0] = 1;
-		int answer = 1;
-		for(int i = 1;i<n;i++) {
-			for(int j = i-1;j>=0;j--) {
-				if(nums[i]>nums[j]) {
-					cnt[i] = Math.max(cnt[i], cnt[j]+1);
-				}
-			}
-			answer = answer<cnt[i]?cnt[i]:answer;
+		
+		int size = 0;
+		
+		for(int i = 0;i<n;i++) {
+			int pos = Arrays.binarySearch(C,0,size,arr[i]);
+			if(pos>=0) continue;
+			
+			int insertPos = Math.abs(pos)-1;
+			C[insertPos] = arr[i];
+			
+			if(insertPos == size) size++;
+			
 		}
-		System.out.println(answer);
+		System.out.println(size);	
 	}
 }
