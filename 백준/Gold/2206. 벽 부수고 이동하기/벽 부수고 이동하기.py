76f698f48820e85699db850f9139e576005f5636
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2023-05-03 14:17:13 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 7576 ms, Memory: 189256 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+
+n,m = map(int,sys.stdin.readline().rstrip().split())
+
+g = [list(map(int,list(sys.stdin.readline().rstrip()))) for _ in range(n)]
+inf = sys.maxsize
+v = [[[inf,inf] for _ in range(m)] for _ in range(n)]
+
+q = deque()
+q.append([0,0,1,0])#x,y,flag
+
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+v[0][0][0] = 1
+while q:
+    a,b,c,f = q.popleft()
+    #print(a,b,c,f)
+    for i in range(4):
+        x = a+dx[i]
+        y = b+dy[i]
+
+        if 0<=x<n and 0<=y<m:
+            if g[x][y] == 0 and v[x][y][f] >c+1:
+                v[x][y][f] = c+1
+                q.append([x,y,c+1,f])
+            elif g[x][y] == 1 and v[x][y][f] > c+1  and f == 0:
+                v[x][y][f] = c+1
+                q.append([x,y,c+1,1])
+if min(v[-1][-1]) == inf:
+    print(-1)
+else:
+    print(min(v[-1][-1]))
