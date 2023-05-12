6e8f107c2b082f7fe95dc378f4c7582e7e14bc93
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2023-05-12 09:05:07 +0900

    [Silver IV] Title: 사이클 단어, Time: 88 ms, Memory: 11940 KB -BaekjoonHub

--- 백준/Silver/1544. 사이클 단어/사이클 단어.java
+++ 백준/Silver/1544. 사이클 단어/사이클 단어.java
@@ -1,35 +1,37 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		ArrayList<String> list = new ArrayList<>();
+		
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		String word;
 
-        int answer = 0;
-
-        HashSet<String> set = new HashSet<>();
-
-        int n = Integer.parseInt(br.readLine());
-
-        String word;
-        int len;
-        while(n-->0) {
-            word = br.readLine();
-
-            if(set.contains(word))
-                continue;
-            answer++;
-
-            len = word.length();
-
-            for(int i = 0;i<len;i++) {
-                set.add(word.substring(i).concat(word.substring(0, i)));
-            }
-        }
-
-        System.out.println(answer);
-    }
-
+		boolean have;
+		
+		while(n-->0) {
+			word = br.readLine();
+			have = false;
+			for(String w: list) {
+				if(word.length()*2==w.length() && w.contains(word)) {
+					have = true;
+					break;
+				}
+					
+			}
+			if(!have) {
+				list.add(word.concat(word));
+			}
+		}
+		
+		System.out.println(list.size());
+	}
 }
