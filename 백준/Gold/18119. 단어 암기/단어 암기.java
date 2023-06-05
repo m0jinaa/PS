6bed2384c9662f38122b4c21a1cc1221f02b3778
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2023-06-05 13:08:32 +0900

    [Gold IV] Title: 단어 암기, Time: 1796 ms, Memory: 71456 KB -BaekjoonHub

---
+++ 백준/Gold/18119. 단어 암기/단어 암기.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[] words = new int[n];
+		int x;
+		char[] word;
+		for (int i = 0; i < n; i++) {
+			x = 0;
+			word = br.readLine().toCharArray();
+			for (char c : word) {
+				x |= (1 << (c - 'a'));
+			}
+			words[i] = x;
+		}
+
+		int status = (1 << 26) - 1;
+		int o, cnt;
+		while (m-- > 0) {
+			st = new StringTokenizer(br.readLine(), " ");
+			o = Integer.parseInt(st.nextToken());
+			x = st.nextToken().charAt(0) - 'a';
+
+			if (o == 1) {
+				status ^= (1 << x);
+			} else {
+				status |= (1 << x);
+			}
+
+			cnt = 0;
+
+			for (int w : words) {
+				if ((status & w) == w) {
+					cnt++;
+				}
+			}
+
+			sb.append(cnt).append("\n");
+		}
+
+		System.out.println(sb.toString());
+
+	}
+}
