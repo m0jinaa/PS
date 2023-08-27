9213ae88bac8af7bb2d34b1f6368a0ac648f969d
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2023-08-27 23:31:22 +0900

    [Bronze I] Title: 진법 변환 2, Time: 80 ms, Memory: 11552 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int n = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+
+		Stack<String> stack = new Stack<>();
+
+		String[] word = new String[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F",
+				"G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+		while (n != 0) {
+			stack.push(word[n % b]);
+			n /= b;
+		}
+
+		while (!stack.isEmpty()) {
+			sb.append(stack.pop());
+		}
+
+		System.out.println(sb.toString());
+	}
+}
