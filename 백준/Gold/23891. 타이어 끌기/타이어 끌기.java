429f5e3478d1f8e8024f883a58ce4478c101de3d
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2023-10-13 15:32:26 +0900

    [Gold II] Title: 타이어 끌기, Time: 136 ms, Memory: 12608 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/23891. 타이어 끌기/타이어 끌기.java
+++ 백준/Gold/23891. 타이어 끌기/타이어 끌기.java
@@ -1,10 +1,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n, m;
+	static int n, m, total;
 
 	static class Tire {
 		int s, p;
@@ -17,29 +16,30 @@ public class Main {
 	}
 
 	static Tire[] tires;
-	static int[] dp;
-	static final int INF = 1_000_000_007;
+	static long[] dp;
 
 	static char getMax() {
 
-		int diff = dp[0];
-
 		for (Tire t : tires) {
 			for (int j = m; j >= t.p; j--) {
-				if (dp[j - t.p] == INF)
-					continue;
-				dp[j] = Math.min(dp[j], dp[j - t.p] - t.s);
+
+				dp[j] = Math.max(dp[j], dp[j - t.p] + t.s);
+
+				if (j >= t.p + 1)
+					dp[j] = Math.max(dp[j], dp[j - (t.p + 1)] + t.s * 2);
 			}
 		}
 
+		long max = 0;
 		for (int i = m; i >= 0; i--) {
-			diff = Math.min(diff, dp[i]);
+			max = Math.max(max, dp[i]);
 		}
 
 		char answer = 'L';
-		if (diff < 0)
+
+		if (max > total)
 			answer = 'W';
-		else if (diff == 0)
+		else if (max == total)
 			answer = 'D';
 
 		return answer;
@@ -55,12 +55,11 @@ public class Main {
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 
-		dp = new int[m + 1];
+		dp = new long[m + 1]; // dp[i] = i명 배치했을 때 얻을 수 있는 최대 점수
 
 		int s, p;
 
-		int total = 0;
-		tires = new Tire[2 * n];
+		tires = new Tire[n];
 
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
@@ -68,17 +67,11 @@ public class Main {
 			s = Integer.parseInt(st.nextToken());
 			p = Integer.parseInt(st.nextToken());
 
-			tires[2 * i] = new Tire(s, p);
-			tires[2 * i + 1] = new Tire(s * 2, p + 1);
+			tires[i] = new Tire(s, p);
 
 			total += s;
 		}
 
-		Arrays.fill(dp, INF);
-
-		dp[0] = total;
-
 		System.out.println(getMax());
-
 	}
 }
