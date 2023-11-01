e112f5029b4464a32539966c31fddd42f6922e2e
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2023-11-01 21:12:50 +0900

    [Gold III] Title: 팩맨, Time: 584 ms, Memory: 253092 KB -BaekjoonHub

---
+++ 백준/Gold/11451. 팩맨/팩맨.java
@@ -0,0 +1,141 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, m;
+	static int[] dx = { 1, -1, 0, 0 };
+	static int[] dy = { 0, 0, 1, -1 };
+	static char[][] map;
+	static char[] dir = { 'S', 'N', 'E', 'W' };
+
+	public static class PackMan {
+		Node a, b;
+		String path;
+		int c;
+
+		public PackMan(Node a, Node b, String path, int c) {
+			this.a = a;
+			this.b = b;
+			this.path = path;
+			this.c = c;
+		}
+	}
+
+	public static class Node {
+		int x;
+		int y;
+
+		public Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	public static String bfs(Node a, Node b) {
+		LinkedList<PackMan> q = new LinkedList<>();
+
+		boolean[][][][] v = new boolean[n][m][n][m];
+
+		q.add(new PackMan(a, b, "", 0));
+
+		String answer = "IMPOSSIBLE";
+
+		while (!q.isEmpty()) {
+			PackMan temp = q.poll();
+
+			if (temp.a.x == temp.b.x && temp.a.y == temp.b.y) {
+				answer = temp.c + " " + temp.path;
+				break;
+			}
+
+			for (int i = 0; i < 4; i++) {
+				int nx1 = temp.a.x + dx[i];
+				int ny1 = temp.a.y + dy[i];
+				int nx2 = temp.b.x + dx[i];
+				int ny2 = temp.b.y + dy[i];
+
+				if (nx1 < 0 || nx1 >= n || ny1 < 0 || ny1 >= m) {
+					if (nx1 < 0)
+						nx1 = n - 1;
+					else if (nx1 >= n)
+						nx1 = 0;
+					else if (ny1 < 0)
+						ny1 = m - 1;
+					else
+						ny1 = 0;
+				}
+
+				if (nx2 < 0 || nx2 >= n || ny2 < 0 || ny2 >= m) {
+					if (nx2 < 0)
+						nx2 = n - 1;
+					else if (nx2 >= n)
+						nx2 = 0;
+					else if (ny2 < 0)
+						ny2 = m - 1;
+					else
+						ny2 = 0;
+				}
+
+				if (map[nx1][ny1] == 'X') {
+					nx1 = temp.a.x;
+					ny1 = temp.a.y;
+				}
+
+				if (map[nx2][ny2] == 'X') {
+					nx2 = temp.b.x;
+					ny2 = temp.b.y;
+				}
+
+				if (v[nx1][ny1][nx2][ny2] || map[nx1][ny1] == 'G' || map[nx2][ny2] == 'G')
+					continue;
+				v[nx1][ny1][nx2][ny2] = true;
+
+				q.add(new PackMan(new Node(nx1, ny1), new Node(nx2, ny2), temp.path + dir[i], temp.c + 1));
+			}
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int t = Integer.parseInt(br.readLine());
+
+		while (t-- > 0) {
+			st = new StringTokenizer(br.readLine(), " ");
+
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+
+			map = new char[n][m];
+
+			Node a = null;
+			Node b = null;
+
+			for (int i = 0; i < n; i++) {
+				map[i] = br.readLine().toCharArray();
+
+				for (int j = 0; j < m; j++) {
+
+					if (map[i][j] == 'P') {
+						if (a == null)
+							a = new Node(i, j);
+						else
+							b = new Node(i, j);
+
+						map[i][j] = '.';
+					}
+				}
+			}
+
+			sb.append(bfs(a, b)).append("\n");
+		}
+
+		System.out.print(sb.toString());
+	}
+}
