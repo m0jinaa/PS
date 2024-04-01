647b177f4c7ed2fed9e044e806d8d054cc90371e
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2024-04-01 15:14:41 +0900

    [Silver V] Title: 귀찮아 (SIB), Time: 208 ms, Memory: 22144 KB -BaekjoonHub

---
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+
+		long answer = 0;
+
+		long sum = 0;
+
+		int x;
+
+		st = new StringTokenizer(br.readLine(), " ");
+
+		for (int i = 0; i < n; i++) {
+			x = Integer.parseInt(st.nextToken());
+
+			answer += x * sum;
+
+			sum += x;
+		}
+
+		System.out.println(answer);
+	}
+}
