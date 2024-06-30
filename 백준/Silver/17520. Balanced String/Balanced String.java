1f158eb6f2c8f5af4b13040039f535f22a93cd15
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2024-06-30 23:21:17 +0900

    [Silver V] Title: Balanced String, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Silver/17520. Balanced String/Balanced String.java
+++ 백준/Silver/17520. Balanced String/Balanced String.java
@@ -7,21 +7,15 @@ public class Main {
 
 		int n = Integer.parseInt(br.readLine());
 
-		int[][] dp = new int[n + 1][3];
-		// dp[n][0]: 1과 0개수 차이 x, dp[n][1] : 1이 하나 더 많음, dp[n][2]: 0이 하나 더 많음.
-
-		dp[1][1] = 1;
-		dp[1][2] = 1;
-
 		final int MOD = 16769023;
 
-		for (int i = 1; i < n; i++) {
-			dp[i + 1][0] = (dp[i][1] + dp[i][2]) % MOD;
-			dp[i + 1][1] = dp[i][0];
-			dp[i + 1][2] = dp[i][0];
-		}
+		int k = (n + 1) / 2;
 
-		int answer = (dp[n][0] + dp[n][1] + dp[n][2]) % MOD;
+		int answer = 1;
+
+		while (k-- > 0) {
+			answer = (answer << 1) % MOD;
+		}
 
 		System.out.println(answer);
 	}
