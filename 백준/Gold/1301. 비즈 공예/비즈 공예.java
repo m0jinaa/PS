61b26c8525d25e1a0702f933e65b5d004641dc3c
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2025-03-21 15:11:59 +0900

    [Gold III] Title: 비즈 공예, Time: 160 ms, Memory: 27028 KB -BaekjoonHub

--- 백준/Gold/1301. 비즈 공예/비즈 공예.java
+++ 백준/Gold/1301. 비즈 공예/비즈 공예.java
@@ -5,35 +5,24 @@ import java.util.Arrays;
 public class Main {
 	static long[][][][][][][] dp;
 
-	static long getCnt(int r, int o, int y, int g, int b, int prev1, int prev2) {
-		if (r + o + y + g + b == 0)
-			return dp[r][o][y][g][b][prev1][prev2] = 1L;
-		else if (dp[r][o][y][g][b][prev1][prev2] != -1)
-			return dp[r][o][y][g][b][prev1][prev2];
+	static long getCnt(int[] marble, int prev1, int prev2) {
+		if (marble[0] + marble[1] + marble[2] + marble[3] + marble[4] == 0)
+			return dp[marble[0]][marble[1]][marble[2]][marble[3]][marble[4]][prev1][prev2] = 1L;
+		else if (dp[marble[0]][marble[1]][marble[2]][marble[3]][marble[4]][prev1][prev2] != -1)
+			return dp[marble[0]][marble[1]][marble[2]][marble[3]][marble[4]][prev1][prev2];
 
 		long cnt = 0;
 
-		if (r != 0 && prev1 != 1 && prev2 != 1) {
-			cnt += getCnt(r - 1, o, y, g, b, prev2, 1);
+		for (int i = 0; i < 5; i++) {
+			if (marble[i] == 0 || prev1 == i + 1 || prev2 == i + 1)
+				continue;
+			marble[i]--;
+			cnt += getCnt(marble, prev2, i + 1);
+			marble[i]++;
+
 		}
 
-		if (o != 0 && prev1 != 2 && prev2 != 2) {
-			cnt += getCnt(r, o - 1, y, g, b, prev2, 2);
-		}
-
-		if (y != 0 && prev1 != 3 && prev2 != 3) {
-			cnt += getCnt(r, o, y - 1, g, b, prev2, 3);
-		}
-
-		if (g != 0 && prev1 != 4 && prev2 != 4) {
-			cnt += getCnt(r, o, y, g - 1, b, prev2, 4);
-		}
-
-		if (b != 0 && prev1 != 5 && prev2 != 5) {
-			cnt += getCnt(r, o, y, g, b - 1, prev2, 5);
-		}
-
-		return dp[r][o][y][g][b][prev1][prev2] = cnt;
+		return dp[marble[0]][marble[1]][marble[2]][marble[3]][marble[4]][prev1][prev2] = cnt;
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -41,29 +30,20 @@ public class Main {
 
 		int n = Integer.parseInt(br.readLine());
 
-		int r = 0;
-		int o = 0;
-		int y = 0;
-		int g = 0;
-		int b = 0;
+		int[] marble = new int[5];
 
-		r = Integer.parseInt(br.readLine());
-		o = Integer.parseInt(br.readLine());
-		y = Integer.parseInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			marble[i] = Integer.parseInt(br.readLine());
+		}
 
-		if (n > 3)
-			g = Integer.parseInt(br.readLine());
-		if (n > 4)
-			b = Integer.parseInt(br.readLine());
-
-		dp = new long[r + 1][o + 1][y + 1][g + 1][b + 1][n + 1][n + 1];
+		dp = new long[marble[0] + 1][marble[1] + 1][marble[2] + 1][marble[3] + 1][marble[4] + 1][n + 1][n + 1];
 
 		// 초기화
-		for (int i = 0; i <= r; i++) {
-			for (int j = 0; j <= o; j++) {
-				for (int w = 0; w <= y; w++) {
-					for (int v = 0; v <= g; v++) {
-						for (int u = 0; u <= b; u++) {
+		for (int i = 0; i <= marble[0]; i++) {
+			for (int j = 0; j <= marble[1]; j++) {
+				for (int w = 0; w <= marble[2]; w++) {
+					for (int v = 0; v <= marble[3]; v++) {
+						for (int u = 0; u <= marble[4]; u++) {
 							for (int z = 0; z <= n; z++) {
 								Arrays.fill(dp[i][j][w][v][u][z], -1);
 							}
@@ -73,7 +53,7 @@ public class Main {
 			}
 		}
 
-		long answer = getCnt(r, o, y, g, b, 0, 0);
+		long answer = getCnt(marble, 0, 0);
 
 		System.out.println(answer);
 	}
