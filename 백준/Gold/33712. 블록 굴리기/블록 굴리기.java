40140e2f762ea4f19a6898b6e97377134f6cbcfa
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2025-05-10 16:23:08 +0900

    [Gold II] Title: 블록 굴리기, Time: 1308 ms, Memory: 391664 KB -BaekjoonHub

--- 백준/Gold/33712. 블록 굴리기/블록 굴리기.java
+++ 백준/Gold/33712. 블록 굴리기/블록 굴리기.java
@@ -1,16 +1,32 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int n, m, k;
 	static int[][] map;
-	static int[][][][] dp;
+	static boolean[][][][] v;
 	static int[][] dx = new int[][] { { -2, 0, 1, 0 }, { -1, 0, 1, 0 }, { -1, 0, 2, 0 } };
 	static int[][] dy = new int[][] { { 0, 1, 0, -2 }, { 0, 2, 0, -1 }, { 0, 1, 0, -1 } };
 	static int[][] ds = new int[][] { { 2, 1, 2, 1 }, { 1, 0, 1, 0 }, { 0, 2, 0, 2 } };
 
+	static class Node {
+		int x, y, c, s;
+
+		public Node(int x, int y, int c, int s) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.c = c;
+			this.s = s;
+		}
+	}
+
+	// (x,y)지점에 s형태로 놓일 수 있는지 확인
+	// s==0 : 세워진 상태 => .
+	// s==1 : 가로로 눞혀진 상태 => ㅡ
+	// s==2 : 세로로 눞혀진 상태 => ㅣ
 	static boolean canPlace(int x, int y, int s) {
 		if (x < 0 || x >= n || y < 0 || y >= m)
 			return false;
@@ -24,31 +40,6 @@ public class Main {
 		return true;
 	}
 
-	// (x,y)지점에 s의 형태로 놓여있고 현재까지 c번 움직였을 때 목표지점에 k번만에 도달할 수 있는지 여부 구하기
-	static int findPossible(int x, int y, int c, int s) {
-		if (dp[x][y][c][s] != -1)
-			return dp[x][y][c][s];
-		else if (c == k) {
-			return dp[x][y][c][s] = 0;
-		}
-		int ret = 0;
-		int nx, ny, ns;
-
-		for (int i = 0; i < 4; i++) {
-			nx = x + dx[s][i];
-			ny = y + dy[s][i];
-			ns = ds[s][i];
-
-			if (!canPlace(nx, ny, ns))
-				continue;
-
-			ret |= findPossible(nx, ny, c + 1, ns);
-
-		}
-
-		return dp[x][y][c][s] = ret;
-	}
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -59,11 +50,11 @@ public class Main {
 		m = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
 
-		dp = new int[n][m][k + 1][3];
+		v = new boolean[n][m][k + 1][3];
 		map = new int[n][m];
 
-		int ex = -1;
-		int ey = -1;
+		int sx = -1;
+		int sy = -1;
 
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
@@ -72,25 +63,50 @@ public class Main {
 				map[i][j] = Integer.parseInt(st.nextToken());
 
 				if (map[i][j] == 2) {
-					ex = i;
-					ey = j;
-				}
-
-				for (int h = 0; h <= k; h++) {
-					Arrays.fill(dp[i][j][h], -1);
+					sx = i;
+					sy = j;
 				}
 			}
 		}
 
-		dp[ex][ey][k][0] = 1;
+		// (x,y)지점에 s의 형태로 놓여있고 현재까지 c번 움직였을 때 목표지점에 k번만에 도달할 수 있는 곳 표시
+		LinkedList<Node> q = new LinkedList<>();
+
+		q.add(new Node(sx, sy, 0, 0));
+		v[sx][sy][0][0] = true;
+
+		Node now;
+		int nx, ny, nc, ns;
+
+		while (!q.isEmpty()) {
+			now = q.poll();
+
+			// k번 움직인 경우 더 움직일 수 없음
+			if (now.c == k)
+				continue;
+
+			for (int i = 0; i < 4; i++) {
+				nx = now.x + dx[now.s][i];
+				ny = now.y + dy[now.s][i];
+				nc = now.c + 1;
+				ns = ds[now.s][i];
+
+				if (!canPlace(nx, ny, ns) || v[nx][ny][nc][ns])
+					continue;
+
+				v[nx][ny][nc][ns] = true;
+
+				q.add(new Node(nx, ny, nc, ns));
+			}
+		}
 
 		int answer = 0;
 
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				if (map[i][j] != 1)
+				if (map[i][j] != 1 || !v[i][j][k][0])
 					continue;
-				answer += findPossible(i, j, 0, 0);
+				answer++;
 			}
 		}
 
