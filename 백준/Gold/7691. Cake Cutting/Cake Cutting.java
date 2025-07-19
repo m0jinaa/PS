3fd2526d0adb83fe7cf6c7b9f8e5e07d9506a47e
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2025-07-19 21:04:18 +0900

    [Gold IV] Title: Cake Cutting, Time: 120 ms, Memory: 15744 KB -BaekjoonHub

--- 백준/Gold/7691. Cake Cutting/Cake Cutting.java
+++ 백준/Gold/7691. Cake Cutting/Cake Cutting.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,46 +7,41 @@ public class Main {
 	static int MAX = 20;
 	static final int INF = 1_000_000_007;
 
+	static int getMin(int x, int y, int z) {
+		if (dp[x][y][z] != 0)
+			return dp[x][y][z];
+		else if (z == 1)
+			return dp[x][y][z] = x * y;
+		else if (x * y == z)
+			return dp[x][y][z] = 1;
+		else if (x * y < z)
+			return dp[x][y][z] = INF;
+
+		int min = INF;
+
+		// 가로로 자르기
+		for (int i = 1; i < x; i++) {
+			for (int j = 1; j < z; j++) {
+				min = Math.min(min, Math.max(getMin(i, y, j), getMin(x - i, y, z - j)));
+			}
+		}
+
+		// 세로로 자르기
+		for (int i = 1; i < y; i++) {
+			for (int j = 1; j < z; j++) {
+				min = Math.min(min, Math.max(getMin(x, i, j), getMin(x, y - i, z - j)));
+			}
+		}
+
+		return dp[x][y][z] = min;
+	}
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 
 		dp = new int[MAX + 1][MAX + 1][MAX * MAX + 1];
-
-		for (int i = 0; i <= MAX; i++) {
-			for (int j = 0; j <= MAX; j++) {
-				Arrays.fill(dp[i][j], INF);
-				dp[i][j][1] = i * j;
-			}
-		}
-
-		// init
-
-		for (int z = 2; z <= MAX * MAX; z++) {
-			for (int x = 1; x <= MAX; x++) {
-				for (int y = 1; y <= MAX; y++) {
-
-					// 가로로 자르기
-					for (int i = 1; i < x; i++) {
-						for (int j = 1; j < z; j++) {
-							if (dp[i][y][j] == INF || dp[x - i][y][z - j] == INF)
-								continue;
-							dp[x][y][z] = Math.min(dp[x][y][z], Math.max(dp[i][y][j], dp[x - i][y][z - j]));
-						}
-					}
-
-					// 세로로 자르기
-					for (int i = 1; i < y; i++) {
-						for (int j = 1; j < z; j++) {
-							if (dp[x][i][j] == INF || dp[x][y - i][z - j] == INF)
-								continue;
-							dp[x][y][z] = Math.min(dp[x][y][z], Math.max(dp[x][i][j], dp[x][y - i][z - j]));
-						}
-					}
-				}
-			}
-		}
 
 		int x, y, z;
 
@@ -61,7 +55,7 @@ public class Main {
 			if (x == 0 && y == 0 && z == 0)
 				break;
 
-			sb.append(dp[x][y][z]).append("\n");
+			sb.append(getMin(x, y, z)).append("\n");
 		}
 
 		System.out.print(sb.toString());
