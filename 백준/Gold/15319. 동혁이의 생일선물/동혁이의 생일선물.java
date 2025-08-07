b1789e557225acf92d2ccd4159e65ccf02e0f8b9
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2025-08-07 14:32:37 +0900

    [Gold IV] Title: 동혁이의 생일선물, Time: 476 ms, Memory: 42820 KB -BaekjoonHub

--- 백준/Gold/15319. 동혁이의 생일선물/동혁이의 생일선물.java
+++ 백준/Gold/15319. 동혁이의 생일선물/동혁이의 생일선물.java
@@ -3,29 +3,17 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static final int MOD = 1_000_000_007;
-
-	static long pow(long x, int t) {
-		if (t == 0)
-			return 1;
-		else if (t % 2 != 0) {
-			return (pow(x, t - 1) * x) % MOD;
-		} else {
-			long nx = pow(x, t / 2);
-
-			return (nx * nx) % MOD;
-		}
-	}
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
+
+		final int MOD = 1_000_000_007;
 
 		int n = Integer.parseInt(br.readLine());
 
 		long x;
-		int t, ind;
+		int t;
+		long v;
 
 		long answer = 0;
 
@@ -34,13 +22,15 @@ public class Main {
 
 			x = Long.parseLong(st.nextToken());
 			t = Integer.parseInt(st.nextToken());
-			ind = 0;
+
+			v = 1;
 
 			while (t > 0) {
 				if (t % 2 != 0) {
-					answer = (answer + pow(x, ind)) % MOD;
+					answer = (answer + v) % MOD;
 				}
-				ind++;
+
+				v = (v * x) % MOD;
 				t >>= 1;
 			}
 		}
