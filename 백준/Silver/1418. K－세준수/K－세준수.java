1207839af79a3275f060d75a7beadfd0f132a779
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2025-08-21 21:45:42 +0900

    [Silver V] Title: K-세준수, Time: 76 ms, Memory: 12136 KB -BaekjoonHub

--- 백준/Silver/1418. K－세준수/K－세준수.java
+++ 백준/Silver/1418. K－세준수/K－세준수.java
@@ -1,66 +1,37 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
-	static int MAX = 1_00_000;
-	static boolean[] isPrime;
-
-	static void init() {
-		isPrime = new boolean[MAX + 1];
-
-		Arrays.fill(isPrime, true);
-
-		isPrime[0] = isPrime[1] = false;
-
-		int limit = (int) Math.sqrt(MAX);
-
-		for (int i = 2; i <= MAX; i++) {
-			if (!isPrime[i])
-				continue;
-			if (i > limit)
-				continue;
-
-			for (int j = i * i; j <= MAX; j += i) {
-				isPrime[j] = false;
-			}
-		}
-	}
-
-	static int maxDivider(int x) {
-
-		int max = 1;
-
-		for (int i = 2; i <= x; i++) {
-			if (!isPrime[i] || x % i != 0)
-				continue;
-			max = Math.max(max, i);
-
-			while (x % i == 0)
-				x /= i;
-		}
-
-		return max;
-	}
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		init();
 
 		int n = Integer.parseInt(br.readLine());
 		int k = Integer.parseInt(br.readLine());
 
+		int[] maxDivider = new int[n + 1];
+
+		maxDivider[1] = 1;
+
+		for (int i = 2; i <= n; i++) {
+			// 소수인지 확인
+			if (maxDivider[i] != 0)
+				continue;
+
+			maxDivider[i] = i;
+
+			for (int j = i * 2; j <= n; j += i) {
+				maxDivider[j] = i;
+			}
+		}
+
 		int answer = 0;
-		int ret;
 
 		for (int i = 1; i <= n; i++) {
-			ret = maxDivider(i);
-			if (ret > k)
+			if (maxDivider[i] == 0 || maxDivider[i] > k)
 				continue;
 			answer++;
 		}
 
 		System.out.println(answer);
-    }
+	}
 }
