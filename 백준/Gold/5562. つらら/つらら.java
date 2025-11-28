1f6bfadc4bf9aa700835f881630b424bafd57ca5
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2025-11-28 14:13:18 +0900

    [Gold III] Title: つらら, Time: 180 ms, Memory: 22104 KB -BaekjoonHub

--- 백준/Gold/5562. つらら/つらら.java
+++ 백준/Gold/5562. つらら/つらら.java
@@ -1,11 +1,35 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
+
+	static int n, l;
+	static int[] icicle;
+	static int[] time;
+
+	static int getTime(int i) {
+		if (time[i] != 0)
+			return time[i];
+
+		int t = 0;
+		int left = 0, right = 0;
+        
+        //왼쪽 고드름이 현재 위치의 고드름보다 긴 경우
+		if (i > 0 && icicle[i - 1] > icicle[i]) {
+			left = getTime(i - 1);
+		}
+        
+        // 오른쪽 고드름이 현재 위치의 고드름보다 긴 경우
+		if (i < n - 1 && icicle[i + 1] > icicle[i]) {
+			right = getTime(i + 1);
+		}
+        
+        // 현재위치의 고드름이 부러지는데 걸리는 시간
+		t = Math.max(left, right) + (l - icicle[i]);
+
+		return time[i] = t;
+	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,56 +37,26 @@ public class Main {
 
 		st = new StringTokenizer(br.readLine(), " ");
 
-		int n = Integer.parseInt(st.nextToken());
-		int l = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		l = Integer.parseInt(st.nextToken());
 
 		// time[i] => i+1번째 고드름이 부러지는데까지 걸리는 시간
-		int[] time = new int[n];
+		time = new int[n];
 
 		// 고드름 길이 저장
-		int[] icicle = new int[n];
+		icicle = new int[n];
 
-		LinkedList<Integer> order = new LinkedList<>();
 		int a;
 
 		for (int i = 0; i < n; i++) {
 			a = Integer.parseInt(br.readLine());
 			icicle[i] = a;
-			order.add(i);
 		}
-
-		// ind를 고드름 길이 내림차순으로 정렬
-		Collections.sort(order, new Comparator<Integer>() {
-
-			@Override
-			public int compare(Integer x, Integer y) {
-				return icicle[y] - icicle[x];
-			}
-
-		});
 
 		int answer = 0;
 
-		int left, right;
-
-		// 길이가 긴 순으로 살펴보면서 현재 위치의 고드름이 부러지는데 걸리는 시간 구하기
-		for (int ind : order) {
-			left = 0;
-			right = 0;
-
-			// 왼쪽에 고드름이 있고, 현재 위치의 고드름보다 긴 경우에만 살펴보기
-			if (ind > 0 && icicle[ind - 1] > icicle[ind]) {
-				left = time[ind - 1];
-			}
-
-			// 오른쪽에 고드름이 있고, 현재 위치의 고드름보다 긴 경우에만 살펴보기
-			if (ind < n - 1 && icicle[ind + 1] > icicle[ind]) {
-				right = time[ind + 1];
-			}
-
-			time[ind] = Math.max(left, right) + (l - icicle[ind]);
-
-			answer = Math.max(time[ind], answer);
+		for (int i = 0; i < n; i++) {
+			answer = Math.max(answer, getTime(i));
 		}
 
 		System.out.println(answer);
