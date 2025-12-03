1c05eb5b91da6e03649983de22501d72c96943ca
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2025-12-03 23:43:44 +0900

    [Silver V] Title: 호텔 방 번호, Time: 72 ms, Memory: 11900 KB -BaekjoonHub

--- 백준/Silver/5671. 호텔 방 번호/호텔 방 번호.java
+++ 백준/Silver/5671. 호텔 방 번호/호텔 방 번호.java
@@ -4,6 +4,40 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+	static final int MAX = 5000;
+	static int[] cnt;
+
+	static void init() {
+		int x, d;
+		boolean[] digit = new boolean[10];
+		boolean possible;
+
+		// cnt[i] => 1~i까지의 수 중에 반복되는 숫자가 없는 수의 개수
+		cnt = new int[MAX + 1];
+
+		for (int i = 1; i <= MAX; i++) {
+			Arrays.fill(digit, false);
+			possible = true;
+			x = i;
+
+			while (x > 0) {
+				d = x % 10;
+				x /= 10;
+
+				if (digit[d]) {
+					possible = false;
+					break;
+				}
+				digit[d] = true;
+			}
+
+			cnt[i] = cnt[i - 1];
+
+			if (possible)
+				cnt[i]++;
+		}
+	}
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -11,40 +45,22 @@ public class Main {
 
 		String input;
 
-		int n, m, x, d, answer;
-		boolean[] digit = new boolean[10];
-		boolean possible;
+		int n, m, answer;
+
+		init();
 
 		while ((input = br.readLine()) != null && !input.isEmpty()) {
+
 			st = new StringTokenizer(input, " ");
 
 			n = Integer.parseInt(st.nextToken());
 			m = Integer.parseInt(st.nextToken());
 
-			answer = 0;
-
-			for (int i = n; i <= m; i++) {
-				Arrays.fill(digit, false);
-				possible = true;
-				x = i;
-
-				while (x > 0) {
-					d = x % 10;
-					x /= 10;
-
-					if (digit[d]) {
-						possible = false;
-						break;
-					}
-					digit[d] = true;
-				}
-
-				if (possible)
-					answer++;
-			}
+			answer = cnt[m] - cnt[n - 1];
 
 			sb.append(answer).append("\n");
 		}
+
 		System.out.print(sb.toString());
 	}
 }
