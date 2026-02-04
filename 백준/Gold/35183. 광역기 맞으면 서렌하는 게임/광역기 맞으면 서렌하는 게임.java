0e11ad41d93c67696e7ebf927368a31c3f8bc459
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2026-02-04 14:54:14 +0900

    [Gold V] Title: 광역기 맞으면 서렌하는 게임, Time: 100 ms, Memory: 12324 KB -BaekjoonHub

--- 백준/Gold/35183. 광역기 맞으면 서렌하는 게임/광역기 맞으면 서렌하는 게임.java
+++ 백준/Gold/35183. 광역기 맞으면 서렌하는 게임/광역기 맞으면 서렌하는 게임.java
@@ -1,45 +1,57 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		final int MAX = 1000;
+
+		final int INF = 1_000_000_007;
 
 		int n = Integer.parseInt(br.readLine());
 
-		boolean[][][] exists = new boolean[n + 1][2][MAX + 1];
-		Arrays.fill(exists[0][0], true);
-
-		int l = -1, r = -1;
-		int s, e;
+		int[] L = new int[n];
+		int[] R = new int[n];
 
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 
-			l = Integer.parseInt(st.nextToken());
-			r = Integer.parseInt(st.nextToken());
-
-			for (int j = 0; j <= MAX; j++) {
-				s = Math.max(0, j - 1);
-				e = Math.min(MAX, j + 1);
-
-				for (int k = s; k <= e; k++) {
-					exists[i + 1][0][k] |= exists[i][0][j] && (l <= k && k <= r);
-					exists[i + 1][1][k] |= (exists[i][0][j]) || (exists[i][1][j] && (l <= k && k <= r));
-				}
-			}
+			L[i] = Integer.parseInt(st.nextToken());
+			R[i] = Integer.parseInt(st.nextToken());
 		}
 
 		boolean survive = false;
 
-		for (int i = 0; i <= MAX; i++) {
-			survive |= exists[n][0][i] || exists[n][1][i];
-		}
+		int s, e;
 
+		boolean possible;
+
+		for (int i = -1; i < n && !survive; i++) {
+			// i번째 광역기를 방패로 막으면 생존가능?
+			// (i == -1 -> 방패 안씀)
+
+			possible = true;
+			s = -INF;
+			e = INF;
+
+			for (int j = 0; j < n; j++) {
+				if (i != j) {
+					s = Math.max(s, L[j]);
+					e = Math.min(e, R[j]);
+
+					if (s > e) {
+						possible = false;
+						break;
+					}
+				}
+
+				s -= 1;
+				e += 1;
+			}
+
+			survive = possible;
+		}
 		System.out.println(survive ? "World Champion\n" : "Surrender\n");
 	}
 }
