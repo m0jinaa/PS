0c45b510ddcfa5c9f587a07d8732ef435c747e59
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2026-03-31 14:42:36 +0900

    [Silver III] Title: 내 선물을 받아줘 2, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/15886. 내 선물을 받아줘 2/내 선물을 받아줘 2.java
+++ 백준/Silver/15886. 내 선물을 받아줘 2/내 선물을 받아줘 2.java
@@ -2,19 +2,41 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
+	static int n;
+	static char[] map;
+	static int[] v;
+
+	static int move(int x, int c) {
+		while (true) {
+			if (v[x] == 0)
+				v[x] = c;
+			else if (v[x] != c) {
+				return 0;
+			} else {
+				return 1;
+			}
+			if (map[x] == 'E') {
+				x++;
+			} else {
+				x--;
+			}
+		}
+	}
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = Integer.parseInt(br.readLine());
+		n = Integer.parseInt(br.readLine());
 
-		char[] map = br.readLine().toCharArray();
+		map = br.readLine().toCharArray();
+		v = new int[n];
 
 		int answer = 0;
 
-		for (int i = 0; i + 1 < n; i++) {
-			if (map[i] == 'E' && map[i + 1] == 'W') {
-				answer++;
-			}
+		for (int i = 0; i < n; i++) {
+			if (v[i] != 0)
+				continue;
+			answer += move(i, i + 1);
 		}
 
 		System.out.println(answer);
