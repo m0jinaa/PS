c8fb83099e7c044e45aa1050f28c1eb659508f8e
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2026-04-26 22:06:46 +0900

    [level 2] Title: 구명보트, Time: 10.86 ms, Memory: 57.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+    
+        int s = 0;
+        int e = people.length-1;
+        
+        while(s<=e){
+            if(people[s]+people[e]<=limit){
+                answer++;
+                s++;
+                e--;
+            }    
+            else{
+                answer++;
+                e--;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
