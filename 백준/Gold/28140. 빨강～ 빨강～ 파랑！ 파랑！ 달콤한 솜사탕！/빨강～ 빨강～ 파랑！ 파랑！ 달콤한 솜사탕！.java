51472c9bd8b9a6ee96b7984409b5607b11d63f55
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2026-04-28 13:24:58 +0900

    [Gold V] Title: 빨강~ 빨강~ 파랑! 파랑! 달콤한 솜사탕!, Time: 1952 ms, Memory: 343228 KB -BaekjoonHub

---
+++ 백준/Gold/28140. 빨강～ 빨강～ 파랑！ 파랑！ 달콤한 솜사탕！/빨강～ 빨강～ 파랑！ 파랑！ 달콤한 솜사탕！.java
@@ -0,0 +1,129 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] R, B;
+
+	static int findR(int s, int e, int x) {// [R[s]~R[e]중 x보다 같거나 큰거 찾기
+		int ret = e + 1;
+
+		int mid;
+
+		while (s <= e) {
+			mid = (s + e) / 2;
+
+			if (R[mid] >= x) {
+				ret = mid;
+				e = mid - 1;
+			} else {
+				s = mid + 1;
+			}
+		}
+
+		return ret;
+	}
+
+	static int findB(int s, int e, int x) {// [B[s]~B[e]중 x보다 같거나 큰거 찾기
+		int ret = e + 1;
+
+		int mid;
+
+		while (s <= e) {
+			mid = (s + e) / 2;
+
+			if (B[mid] >= x) {
+				ret = mid;
+				e = mid - 1;
+			} else {
+				s = mid + 1;
+			}
+		}
+
+		return ret;
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine(), " ");
+
+		int n = Integer.parseInt(st.nextToken());
+		int q = Integer.parseInt(st.nextToken());
+
+		char[] S = br.readLine().toCharArray();
+
+		R = new int[n];
+		B = new int[n];
+
+		int rInd = 0;
+		int bInd = 0;
+
+		for (int i = 0; i < n; i++) {
+			if (S[i] == 'R') {
+				R[rInd++] = i;
+			} else if (S[i] == 'B') {
+				B[bInd++] = i;
+			}
+		}
+
+		// a,b,c,d 찾기
+
+		int l, r;
+
+		int a = -1, b = -1, c = -1, d = -1;
+		boolean possible;
+
+		while (q-- > 0) {
+
+			st = new StringTokenizer(br.readLine(), " ");
+
+			l = Integer.parseInt(st.nextToken());
+			r = Integer.parseInt(st.nextToken());
+
+			possible = true;
+
+			a = findR(0, rInd - 1, l);
+
+			if (a == rInd || R[a] > r) {
+				possible = false;
+			} else {
+				a = R[a];
+
+				b = findR(0, rInd - 1, a + 1);
+
+				if (b == rInd || R[b] > r) {
+					possible = false;
+				} else {
+					b = R[b];
+
+					c = findB(0, bInd - 1, b + 1);
+
+					if (c == bInd || B[c] > r) {
+						possible = false;
+					} else {
+						c = B[c];
+
+						d = findB(0, bInd - 1, c + 1);
+
+						if (d == bInd || B[d] > r) {
+							possible = false;
+						} else {
+							d = B[d];
+						}
+					}
+				}
+			}
+
+			if (possible) {
+				sb.append(a).append(" ").append(b).append(" ").append(c).append(" ").append(d).append("\n");
+			} else {
+				sb.append("-1\n");
+			}
+		}
+
+		System.out.println(sb.toString());
+	}
+}
