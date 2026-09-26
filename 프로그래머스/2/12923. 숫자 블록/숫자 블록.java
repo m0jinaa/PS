085d64edd5285752fb27c423c418fd56bbfb44e1
m0jinaa <96249562+m0jinaa@users.noreply.github.com>
2026-09-26 23:32:49 +0900

    [level 2] Title: 숫자 블록, Time: 93.53 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -1,31 +1,18 @@
 class Solution {
-    int getDivider(long x){
-        int ret = 1;
-        
-        int limit = (int)Math.sqrt(x);
-        
-        for(int i = 2;i<=limit;i++){
-            if(x%i == 0){
-                if(x/i>10000000){
-                    ret = Math.max(ret,i);
-                }
-                else{
-                    ret = (int)(x/i);
-                    break;
-                }
-            }
-        }
-        
-        return x == 1?0:ret;
-    }
-    
     public int[] solution(long begin, long end) {
         int l = (int)(end-begin+1);
         
         int[] answer = new int[l];
         
-        for(int i = 0;i<l;i++){
-            answer[i] = getDivider(begin+i);
+        int s;
+        
+        for(int i = 1;i*2<=end;i++){
+            s = Math.max(i*2,(int)Math.ceil(begin*1.0/i)*i);
+            
+            while(s<=end){
+                answer[(int)(s-begin)] = i;
+                s+=i;
+            }
         }
         
         return answer;
